fa2d173d8c32dacf4f3cd336b98bcdbaf8e345b3
DESKTOP-0PM7LPE\jihyeon <osye1128@naver.com>
2021-02-19 23:01:28 +0900

    mainImage완성

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+export const API_URL='https://api.themoviedb.org/3/'
+export const API_KEY='024c5aeda6e557f6013a7bd1dbd1c4c3';
+export const IMAGE_BASE_URL='http://image.tmdb.org/t/p/';
+
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import { FaCode } from "react-icons/fa";
+import { API_KEY, API_URL,IMAGE_BASE_URL } from '../../Config';
+import MainImage from './Sections/MainImage';
 
 function LandingPage() {
+    const [Movies, setMovies] = useState([]);
+    const [MainMovieImage, setMainMovieImage] = useState(null);
+    
+    useEffect(() => {
+        const endPoint=`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+        fetch(endPoint)
+        .then(response=>response.json())
+        .then(response=>{
+            setMovies([response.results]);
+            setMainMovieImage(response.results[0]);
+        })
+    }, [])
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div style={{width:'100%' ,margin:'0'}}>
+            {MainMovieImage &&
+            <MainImage image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+                title={MainMovieImage.original_title}
+                text={MainMovieImage.overview}
+            />
+            }
+            <div>
+                <h2>Movies by latest</h2>
+                <hr />
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+            <div>
+                <button>Load More</button>
+            </div>
+        </div>
     )
 }
 
